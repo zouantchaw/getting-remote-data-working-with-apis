f2fddaa3a609ba423c29fe1d23bdbbe62e2839c9
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-04-12 14:00:14 -0400

    Done.

--- nyc_api.rb
+++ nyc_api.rb
@@ -13,6 +13,7 @@ require 'net/http'
   end
 
   def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
     programs = JSON.parse(self.get_programs)
     programs.collect do |program|
       program["agency"]
@@ -21,5 +22,8 @@ require 'net/http'
 
 end
 
- programs = GetPrograms.new.get_programs
- puts programs
+ #programs = GetPrograms.new.get_programs
+ #puts programs
+
+ programs = GetPrograms.new
+ puts programs.program_school.uniq
